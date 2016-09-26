37aca303fd4852fcf5410ffb74014885fd907979
Zhi Li <lizhiblack@gmail.com>
2016-09-26 02:47:56 -0400

    update files

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,3 @@
-"""
-Write a recursive function isPalindrome(string) that returns True if
-string is a palindrome, that is, a word that is the same when reversed.
-Examples of palindromes are “deed”, “rotor”, or “aibohphobia”. Hint: A
-word is a palindrome if the frst and last letters match and the
-remainder is also a palindrome.
-"""
-
-
 def isPalindrome(s):
     pass
 
